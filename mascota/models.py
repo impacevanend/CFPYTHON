8bf752cd708d03a5d4726714d4ca3abff72e891d
Joselo <impacevanend@gmail.com>
2021-10-15 17:46:46 -0500

    creación de aplicación refugio

---
+++ mascota/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Macota(models.Model):
+    nombre = models.CharField(max_length =50)
+    sexo = models.CharField(max_length =10)
+    edad_aproximada = models.IntegerField()
+    fecha_rescate = models.DateField()

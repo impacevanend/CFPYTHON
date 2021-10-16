f9d1d51b3f24e1111d64c3a95ba94b3fa9023791
Joselo <impacevanend@gmail.com>
2021-10-15 19:48:02 -0500

    iformacion de shell, queryset

--- adopcion/models.py
+++ adopcion/models.py
@@ -6,6 +6,5 @@ class Persona(models.Model):
     nombre = models.CharField(max_length = 50)
     apellidos = models.CharField(max_length = 50)
     edad = models.IntegerField()
-    edad = models.CharField(max_length = 12)
     email = models.EmailField()
     domicilio = models.TextField()    
--- mascota/admin.py
+++ mascota/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from mascota.models import Mascota, Vacunas
 
 # Register your models here.
+admin.site.register(Mascota)
+admin.site.register(Vacunas)
